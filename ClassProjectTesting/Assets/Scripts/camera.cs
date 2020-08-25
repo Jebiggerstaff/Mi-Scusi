25fef27571899032b3d368757ee8d5703b6a05d1
AJBollman <46943559+AJBollman@users.noreply.github.com>
2020-08-25 16:58:38 -0500

    returned the game back to the github

---
+++ ClassProjectTesting/Assets/Scripts/camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform Player;
+    public Vector3 Offset;
+
+    void LateUpdate()
+    {
+        if (Player != null)
+            transform.position = Player.position + Offset;
+    }
+}
