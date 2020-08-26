8584ed8d613b4cf6b58e2e42465b2769936b50aa
snberanek <36345444+snberanek@users.noreply.github.com>
2020-08-25 20:55:00 -0500

    tutorial messages
    made some quick and dirty tutorial popups

---
+++ Assets/Scripts/TutorialMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessage : MonoBehaviour
+{
+    public GameObject TutorialMessgaeObject;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            TutorialMessgaeObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            TutorialMessgaeObject.SetActive(false);
+        }
+    }
+
+}
