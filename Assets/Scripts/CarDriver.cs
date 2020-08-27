5e716bc006acd1937b451d129c4c855cc6422b4b
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-08-27 15:03:35 -0500

    Car Driver

--- Assets/Scripts/CarDriver.cs
+++ Assets/Scripts/CarDriver.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class CarDriver : MonoBehaviour
 {
-    float Road
+    public GameObject[] RoadPoints = new GameObject[0];
+    public float carSpeed = 0f;
 
+    private Vector3 currentLocation;
+    private int targetPoint=0;
 
+    private void Start()
+    {
+        transform.position = RoadPoints[0].transform.position;
+    }
+
+    void Update()
+    {
+        currentLocation = this.gameObject.transform.position;
+
+        transform.LookAt(RoadPoints[targetPoint].transform);
+
+        if (currentLocation != RoadPoints[targetPoint].transform.position)
+            transform.position = Vector3.MoveTowards(transform.position, RoadPoints[targetPoint].transform.position, carSpeed);
+
+        else
+        {
+            if (targetPoint + 1 != RoadPoints.Length)
+                targetPoint += 1;
+            else
+                targetPoint = 0;
+        }
+
+        
+    }
 }
