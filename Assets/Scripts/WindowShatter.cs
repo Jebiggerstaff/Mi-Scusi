63eb70241d821ba91abb83c7a89378fc071c71e5
snberanek <36345444+snberanek@users.noreply.github.com>
2020-08-31 15:07:43 -0500

    windows shatter
    got breakable windows

---
+++ Assets/Scripts/WindowShatter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowShatter : MonoBehaviour
+{
+
+    public GameObject destroyedVersion;
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CanBeGrabbed")
+        {
+           
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        
+    }
+}
