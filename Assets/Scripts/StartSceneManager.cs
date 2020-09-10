6c569c7ed7e49eb07bedb22e0c0eebb1528791f3
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2020-09-10 13:39:02 -0500

    YES

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        doStuff();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void doStuff()
+    {
+        var os = FindObjectOfType<OverlayScene>();
+        if (os == null)
+        {
+            os = Instantiate(overlayStuff).GetComponent<OverlayScene>();
+        }
+
+        if (isMenuScene)
+        {
+            os.player.SetActive(false);
+            os.menu.SetActive(true);
+        }
+        else
+        {
+            os.player.SetActive(true);
+            os.menu.SetActive(false);
+            os.player.transform.position = playerSpawnLocation;
+        }
+    }
+
+    public GameObject overlayStuff;
+    public bool isMenuScene = false;
+    public Vector3 playerSpawnLocation;
+}
