fbc66920f63852f801dd80a4b279407b5fe96284
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2020-09-10 13:52:33 -0500

    YESSSS

--- Assets/Scripts/OverlayScene.cs
+++ Assets/Scripts/OverlayScene.cs
@@ -7,26 +7,48 @@ public class OverlayScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        cancelDelayCount = 0;
         DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cancelDelayCount > 0)
+        {
+            cancelDelayCount -= Time.unscaledDeltaTime;
+
+        }
+        else
+        {
+            CancelDelay = false;
+        }
+
         if (Input.GetAxis("Cancel") == 1)
         {
-            if (menu.activeSelf == false)
+            if (menu.activeSelf == false && !CancelDelay)
             {
-                player.SetActive(false);
+
+                CancelDelay = true;
+                cancelDelayCount = 0.1f;
+
+                //player.SetActive(false);
                 menu.SetActive(true);
                 menu.GetComponent<SpeedTutorMainMenuSystem.MenuController>().needResumeButton = true;
                 menu.GetComponent<SpeedTutorMainMenuSystem.MenuController>().onEnable();
+
             }
         }
 
     }
 
+
     public GameObject player;
     public GameObject menu;
+
+    public bool CancelDelay;
+    public float cancelDelayCount;
+
+
 
 }
--- Assets/SpeedTutor_Full_Menu_System/MenuScripts/MenuController.cs
+++ Assets/SpeedTutor_Full_Menu_System/MenuScripts/MenuController.cs
@@ -35,6 +35,7 @@ namespace SpeedTutorMainMenuSystem
         [SerializeField] private GameObject confirmationMenu;
         [SerializeField] private GameObject customizationMenu;
         [SerializeField] private GameObject loadingScreenCanvas;
+        [SerializeField] private GameObject ResumeGameBtn;
         [Space(10)]
         [Header("Menu Popout Dialogs")]
         [SerializeField] private GameObject noSaveDialog;
@@ -61,14 +62,17 @@ namespace SpeedTutorMainMenuSystem
         #endregion
 
         float originalGameTime;
+        float oldAxis;
 
         [Header("SceneTransitions")]
-        public GameObject overlayScene;
+        public OverlayScene overlayScene;
+        public bool needResumeButton = false;
 
 
         #region Initialisation - Button Selection & Menu Order
         private void Start()
         {
+
             menuNumber = 1;
             originalGameTime = Time.timeScale;
             Time.timeScale = 0.0001f;
@@ -82,16 +86,15 @@ namespace SpeedTutorMainMenuSystem
             yield return new WaitForSeconds(2);
             confirmationMenu.SetActive(false);
         }
-        
-        void onEnable()
+
+        public void onEnable()
         {
-            menuNumber = 1;
             originalGameTime = Time.timeScale;
             Time.timeScale = 0.0001f;
         }
         void onDisable()
         {
-            Time.timeScale = originalGameTime;
+
         }
 
         private void Update()
@@ -99,8 +102,11 @@ namespace SpeedTutorMainMenuSystem
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
 
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetAxis("Cancel") > oldAxis && !overlayScene.CancelDelay)
             {
+                overlayScene.CancelDelay = true;
+                overlayScene.cancelDelayCount = 0.1f;
+
                 if (menuNumber == 2 || menuNumber == 7 || menuNumber == 8 || menuNumber == 9 || menuNumber == 10)
                 {
                     GoBackToMainMenu();
@@ -118,6 +124,20 @@ namespace SpeedTutorMainMenuSystem
                     GoBackToGameplayMenu();
                     ClickSound();
                 }
+                else if (menuNumber == 1)
+                {
+                    if (needResumeButton)
+                    {
+                        resumeGame();
+                    }
+                }
+            }
+
+            oldAxis = Input.GetAxis("Cancel");
+
+            if (ResumeGameBtn.activeSelf == false && needResumeButton)
+            {
+                ResumeGameBtn.SetActive(true);
             }
         }
 
@@ -184,7 +204,7 @@ namespace SpeedTutorMainMenuSystem
                 menuNumber = 7;
             }
 
-            if(buttonType == "Achievements")
+            if (buttonType == "Achievements")
             {
                 menuDefaultCanvas.SetActive(false);
                 AchievementsCanvas.SetActive(true);
@@ -308,19 +328,29 @@ namespace SpeedTutorMainMenuSystem
         {
             //TurnOnLoadingScreen();
 
+            GoBackToMainMenu();
+
             Time.timeScale = originalGameTime;
 
-            AsyncOperation async = SceneManager.LoadSceneAsync(_newGameButtonLevel, LoadSceneMode.Additive);
-            
+            AsyncOperation async = SceneManager.LoadSceneAsync(_newGameButtonLevel);
+
             while (!async.isDone)
             {
                 progressSlider.value = async.progress;
                 yield return null;
             }
 
-            //SceneManager.MoveGameObjectToScene(OverlayScene, _newGameButtonLevel);
+
 
         }
+        public void resumeGame()
+        {
+            needResumeButton = false;
+            Time.timeScale = 1;
+            overlayScene.player.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
         public void ClickLoadGameDialog(string ButtonType)
         {
             if (ButtonType == "Yes")
@@ -353,7 +383,7 @@ namespace SpeedTutorMainMenuSystem
         public void LoadCharacter()
         {
             var modelLoader = FindObjectOfType<CharacterModelLoader>();
-            if(modelLoader != null)
+            if (modelLoader != null)
             {
                 modelLoader.LoadAllModels();
             }
@@ -410,6 +440,16 @@ namespace SpeedTutorMainMenuSystem
             customizationMenu.SetActive(false);
             loadingScreenCanvas.SetActive(false);
             menuNumber = 1;
+
+            if (needResumeButton)
+            {
+                ResumeGameBtn.SetActive(true);
+
+            }
+            else
+            {
+                ResumeGameBtn.SetActive(false);
+            }
         }
 
         public void TurnOnLoadingScreen()
