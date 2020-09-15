cddf3dad62f22c1f565ab4d3651dab14ddbccadc
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2020-09-15 13:11:40 -0500

    stuff

---
+++ Assets/Scripts/NavMeshFollowClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshFollowClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                foreach(var x in FindObjectsOfType<NavMeshAgent>())
+                {
+                    x.SetDestination(hit.point);
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var x in FindObjectsOfType<NavMeshAgent>())
+            {
+                x.enabled = !x.enabled;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NewAIMan.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewAIMan : MonoBehaviour
+{
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNewDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, currentDestination) < 3)
+        {
+            SetNewDestination();
+        }
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if collision is player
+        agent.enabled = false;
+
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //if collision is player
+        agent.enabled = true;
+        agent.SetDestination(currentDestination);
+    }
+
+
+    public void SetNewDestination()
+    {
+        SetNewDestination(destinations[Random.Range(0, destinations.Count)]);
+    }
+    public void SetNewDestination(Vector3 target)
+    {
+        currentDestination = target;
+        agent.SetDestination(target);
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public Vector3 currentDestination;
+    public List<Vector3> destinations;
+    NavMeshAgent agent;
+}
