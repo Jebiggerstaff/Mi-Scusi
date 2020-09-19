2d9640ad85a65402151f54a9c8e573000a8b03b6
snberanek <36345444+snberanek@users.noreply.github.com>
2020-09-18 22:45:26 -0500

    Noir Progress
    Noir is pretty much laid out. I just need to fill it with stuff and create the elaborate murder mystery

---
+++ Assets/Scripts/CasinoWheel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasinoWheel : MonoBehaviour
+{
+ 
+    public GameObject unhideThisOne;
+    public GameObject hideThisOne;
+    int Count = 0;
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickaxe" || other.tag == "Gold" || other.tag == "Shovel" )
+        {
+            Count = Count + 1;
+        
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pickaxe" || other.tag == "Gold" || other.tag == "Shovel")
+        {
+            Count = Count - 1;
+
+        }
+    }
+
+    void Update()
+    {
+        if (Count >= 3)
+        {
+            unhideThisOne.SetActive(true);
+            hideThisOne.SetActive(false);
+        }
+
+    }
+    
+
+  
+}
