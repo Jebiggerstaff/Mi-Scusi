50a77de44330f145eb77bb63489e0a92945a808a
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2020-09-21 08:49:31 -0500

    AI Manager

--- Assets/Scripts/NewAI/AIManager.cs
+++ Assets/Scripts/NewAI/AIManager.cs
@@ -7,7 +7,7 @@ public class AIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateAllAI();
     }
 
     // Update is called once per frame
