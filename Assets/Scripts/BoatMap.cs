d91d32e2fc031b2d5c77e2fd31e3d869c2b54287
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-09-22 13:58:01 -0500

    BOAT

---
+++ Assets/Scripts/BoatMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMap : MonoBehaviour
+{
+    public GameObject steeringWheel;
+    public float rotSpeed;
+
+    void FixedUpdate()
+    {
+        Debug.Log(steeringWheel.transform.localRotation.eulerAngles.x);
+
+        if (steeringWheel.transform.rotation.x > -85)
+        {
+            this.transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.eulerAngles.y + rotSpeed, transform.rotation.z,transform.rotation.w);
+        }
+        else if (steeringWheel.transform.rotation.x < -95)
+        {
+            this.transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.eulerAngles.y - rotSpeed, transform.rotation.z, transform.rotation.w);
+        }
+
+
+    }
+}
