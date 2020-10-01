d0c2a7c5be3f3e86bc4716512f4bf5baa448d5a8
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-10-01 12:52:31 -0500

    Tutorial popups match contoller

---
+++ Assets/TutorialDevice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDevice : MonoBehaviour
+{
+
+    public APRController con;
+    public GameObject TutorialKeyboard;
+    public GameObject TutorialController;
+
+    void Update()
+    {
+        if (con.usingController)
+        {
+            TutorialController.SetActive(true);
+        }
+        else
+        {
+            TutorialKeyboard.SetActive(true);
+        }
+    }
+}
