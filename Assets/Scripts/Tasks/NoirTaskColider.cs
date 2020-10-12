12643feb3b05641383841b9d1e7d0ca0ca563bc0
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-10-12 15:19:53 -0500

    Noir Murder Tasks

---
+++ Assets/Scripts/Tasks/NoirTaskColider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NoirTaskColider : MonoBehaviour
+{
+    public NoirTaskManager NoirTaskManager;
+
+    private bool SuspectIdentified = false;
+    private bool WeaponIdentified = false;
+    private bool PosterDelivered = false;
+
+    public void Start()
+    {
+        NoirTaskManager = GameObject.Find("TaskUI").GetComponent<NoirTaskManager>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (this.name == "MurderScene")
+        {
+            foreach (GameObject suspect in NoirTaskManager.Suspects)
+            {
+                if (other == suspect && SuspectIdentified == false)
+                {
+                    SuspectIdentified = true;
+                    //have the AI say some shit
+                    NoirTaskManager.TaskCompleted("MurderSuspect");
+                }
+            }
+
+            if (other.name == NoirTaskManager.MurderWeapon.name && WeaponIdentified == false)
+            {
+                WeaponIdentified = true;
+                //have the AI say some other shit
+                NoirTaskManager.TaskCompleted("MurderWeapon");
+            }
+
+            if (other.name == "WantedPoster" && PosterDelivered == false)
+            {
+                PosterDelivered = true;
+                //have the AI say some other shit
+                NoirTaskManager.TaskCompleted("WantedPoster");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tasks/NoirTaskManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoirTaskManager : MonoBehaviour
+{
+    [Header("Gameobjects")]
+    public GameObject Player;
+    public GameObject TaskCompleteText;
+    public GameObject TaskList;
+
+    [Header("Tasks")]
+    public GameObject[] Tasks = new GameObject[0];
+
+    [Header("Murder GameObjects")]
+    public GameObject[] Suspects = new GameObject[0];
+    public GameObject MurderWeapon;
+
+    void Start()
+    {
+        this.transform.GetChild(0).GetComponent<Canvas>().worldCamera = Camera.main;
+        this.transform.GetChild(0).GetComponent<Canvas>().planeDistance = .2f;
+        Player = GameObject.Find("FinalPlayer");
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("TaskMenu"))
+        {
+            TaskCompleteText.SetActive(false);
+            TaskList.SetActive(true);
+        }
+        if (Input.GetButtonUp("TaskMenu"))
+            TaskList.SetActive(false);
+    }
+
+    public void TaskCompleted(string Task)
+    {
+        TaskCompleteText.SetActive(true);
+        switch (Task)
+        {
+            case "MurderWeapon":
+                Tasks[0].SetActive(true);
+                break;
+            case "MurderSuspect":
+                Tasks[1].SetActive(true);
+                break;
+            case "WantedPoster":
+                Tasks[2].SetActive(true);
+                break;
+        }
+
+    }
+
+}
