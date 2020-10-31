1a4668524f96e10fd6e66f8e988312d012451bc7
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-10-31 16:32:04 -0500

    Office Tasks Started

---
+++ Assets/Scripts/Tasks/OfficeTaskCollider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeTaskCollider : MonoBehaviour
+{
+
+    public OfficeTaskManager OfficeTaskManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OfficeTaskManager = GameObject.Find("TaskUI").GetComponent<OfficeTaskManager>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        #region WaterCooler
+        if (other.gameObject == OfficeTaskManager.WaterCoolerJug && name == "WaterCooler")
+        {
+            OfficeTaskManager.TaskCompleted("RefilWater");
+        }
+        #endregion
+        //Code to talk to the Receptionist
+        //probably needs to be done in the talking script
+        #region CoffeeToBoss
+        if (other.name== "CoffeeMug" && name == "Boss")
+        {
+            OfficeTaskManager.TaskCompleted("GetBossCoffee");
+        }
+        #endregion
+        #region CopyButt
+        if (other.name == "APR_Head" && name == "Copy Machine")
+        {
+            OfficeTaskManager.TaskCompleted("CopyButt");
+        }
+        #endregion
+    }
+
+}
---
+++ Assets/Scripts/Tasks/OfficeTaskManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficeTaskManager : MonoBehaviour
+{
+    //Public Variables
+    [Header("Tasks")]
+    public GameObject[] Tasks = new GameObject[0];
+
+    [Header("Gameobjects")]
+    public GameObject Player;
+    public GameObject TaskCompleteText;
+    public GameObject TaskList;
+
+    [Header("Task Objects")]
+
+    public GameObject WaterCoolerJug;
+    public GameObject Coffee;
+
+
+    public void Start()
+    {
+        this.transform.GetChild(0).GetComponent<Canvas>().worldCamera = Camera.main;
+        this.transform.GetChild(0).GetComponent<Canvas>().planeDistance = .2f;
+        Player = GameObject.Find("FinalPlayer");
+        //Debug.Log("Found Player: " + Player);
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("TaskMenu"))
+        {
+            TaskCompleteText.SetActive(false);
+            TaskList.SetActive(true);
+        }
+        if (Input.GetButtonUp("TaskMenu"))
+            TaskList.SetActive(false);
+    }
+
+    public void TaskCompleted(string Task)
+    {
+        TaskCompleteText.SetActive(true);
+        switch (Task)
+        {
+            case "RefilWater":
+                Tasks[0].SetActive(true);
+                break;
+            case "HelpCoworker":
+                Tasks[1].SetActive(true);
+                break;
+            case "GetBossCoffee":
+                Tasks[2].SetActive(true);
+                break;
+            case "CopyButt":
+                Tasks[3].SetActive(true);
+                break;
+        }
+
+    }
+}
