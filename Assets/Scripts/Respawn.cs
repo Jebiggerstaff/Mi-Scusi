fa68eb0a3ede0d244da659793c7399779d1feca1
AJBollman <alec.bollman@trojans.dsu.edu>
2020-11-12 15:58:32 -0600

    stuff

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject SpawnPoint;
+    public float RespawnHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.transform.position.y < RespawnHeight)
+        {
+            Player.transform.position = SpawnPoint.transform.position;
+        }
+    }
+}
