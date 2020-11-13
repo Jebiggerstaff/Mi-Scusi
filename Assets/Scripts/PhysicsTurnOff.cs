fb085f8dbcbd996ead7b556fbdf59499ba254dad
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2020-11-13 13:57:46 -0600

    physics turn off fix

--- Assets/Scripts/PhysicsTurnOff.cs
+++ Assets/Scripts/PhysicsTurnOff.cs
@@ -13,24 +13,31 @@ public class PhysicsTurnOff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(allRigidbodies != null)
+        if(allRigidbodies != null && allRigidbodies.Count > 0)
         {
-            foreach(var rb in allRigidbodies)
+
+            Debug.Log("Total Rigidbodies: " + allRigidbodies.Count);
+            for (int i = 0; i < 20; i++)
             {
-                if(Vector3.Distance(player.position, rb.transform.position) > distance)
+                var rb = allRigidbodies[currentCount];
+                if(rb == null || rb.GetComponent<NewAIMan>() != null)
                 {
-                    rb.detectCollisions = false;
-                    if(rb.GetComponent<NewAIMan>() == null)
-                    {
-                        rb.isKinematic = true;
-                    }
+                    allRigidbodies.Remove(rb);
                 }
                 else
                 {
-                    rb.detectCollisions = true;
-                    if (rb.GetComponent<NewAIMan>() == null)
+                    if (Vector3.Distance(player.position, rb.transform.position) > distance)
                     {
-                        rb.isKinematic = false;
+                        //rb.detectCollisions = false;
+                        if (rb.GetComponent<NewAIMan>() == null)
+                        {
+                            rb.isKinematic = true;
+                        }
+                    }
+                    currentCount++;
+                    if(currentCount >= allRigidbodies.Count)
+                    {
+                        currentCount = 0;
                     }
                 }
             }
@@ -42,10 +49,27 @@ public class PhysicsTurnOff : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         allRigidbodies = new List<Rigidbody>(FindObjectsOfType<Rigidbody>());
+        
+        currentCount = 0;
+        Rigidbody rb;
+        while (currentCount < allRigidbodies.Count)
+        {
+            rb = allRigidbodies[currentCount];
+            if (rb == null || rb.GetComponent<NewAIMan>() != null)
+            {
+                allRigidbodies.Remove(rb);
+            }
+            else
+            {
+                currentCount++;
+            }
+        }
+        currentCount = 0;
 
     }
 
     List<Rigidbody> allRigidbodies = null;
     public float distance = 150f;
     public Transform player;
+    public int currentCount = 0;
 }
