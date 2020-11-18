a774e7f0a8f13ac7df8a044c57c3b33fe1b2f33b
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-11-18 08:34:30 -0600

    tASKS booiiiiii

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class NPC : MonoBehaviour {
+
+    ItalyTaskManager ItalyTaskManager = new ItalyTaskManager();
 
     public Transform ChatBackGround;
     public Transform NPCCharacter;
 
     private DialogueSystem dialogueSystem;
 
-    
-
     public string Name;
-
-
 
     [TextArea(5, 10)]
     public string[] sentences;
 
     void Start () {
+        if(SceneManager.GetActiveScene().name == "Italy")
+            ItalyTaskManager = GameObject.Find("TaskUI").GetComponent<ItalyTaskManager>();
         dialogueSystem = FindObjectOfType<DialogueSystem>();
     }
 	
@@ -34,16 +35,19 @@ public class NPC : MonoBehaviour {
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player") { 
-        this.gameObject.GetComponent<NPC>().enabled = true;
-        FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
-        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.F))
-        {
             this.gameObject.GetComponent<NPC>().enabled = true;
-            dialogueSystem.Names = Name;
-            dialogueSystem.dialogueLines = sentences;
-            FindObjectOfType<DialogueSystem>().NPCName();
+            FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
+            if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.F))
+            {
+                if (Name == "Former Gangster")
+                    ItalyTaskManager.TaskCompleted("HearAboutMafia");
+
+                this.gameObject.GetComponent<NPC>().enabled = true;
+                dialogueSystem.Names = Name;
+                dialogueSystem.dialogueLines = sentences;
+                FindObjectOfType<DialogueSystem>().NPCName();
+            }
         }
-    }
     }
 
     public void OnTriggerExit()
