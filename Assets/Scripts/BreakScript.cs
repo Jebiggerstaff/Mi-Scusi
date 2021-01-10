f37da1407e5bdb2049165975d2fdee700e7ecb71
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-10 15:01:07 -0600

    break stuff
    stuff i did over break. didnt have wifi until now

---
+++ Assets/Scripts/BreakScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakScript : MonoBehaviour
+{
+    public GameObject Unbroken;
+    public GameObject Broken;
+    public ParticleSystem poof;
+    public AudioSource sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickaxe"))
+        {
+            poof.Play();
+            sound.Play();
+            Unbroken.SetActive(false);
+            Broken.SetActive(true);
+        }
+    }
+}
