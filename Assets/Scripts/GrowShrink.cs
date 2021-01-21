a439724bb690ce68ab0791f47174c4f3f26b4d2b
AJBollman <alec.bollman@trojans.dsu.edu>
2021-01-21 15:26:27 -0600

    editing stuff before i break it

---
+++ Assets/Scripts/GrowShrink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowShrink : MonoBehaviour
+{
+    public bool grow = false;
+    public bool shrink = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (grow == true)
+        {
+            other.transform.localScale = new Vector3(8, 8, 8);
+        }
+
+        if(shrink == true)
+        {
+            other.transform.localScale = new Vector3(1, 1, 1);
+        }
+        
+    }
+}
