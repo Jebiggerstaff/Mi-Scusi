3aa5289311b8d0af1bf88ec017710f22a3caf5cd
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-26 12:32:46 -0600

    tutorial and transition things
    messed with the tutorial and the scene transitions

---
+++ Assets/Scripts/UI/LoadLevelButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadLevelButton : MonoBehaviour
+{
+    public int index = 4;
+
+    public GameObject nextpic;
+
+    public int Trans = 1;
+
+    public void TaskOnClick()
+    {
+        if (Trans == 1)
+        {
+            nextpic.SetActive(true);
+        }
+
+        else if (Trans == 2)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+        }
+    }
+
+    public void Update()
+    {
+        if (Trans == 2)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+        }
+    }
+}
