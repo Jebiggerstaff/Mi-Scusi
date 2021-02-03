7970f7728c8f182285316b3427236b882068c217
AJBollman <alec.bollman@trojans.dsu.edu>
2021-02-03 13:46:02 -0600

    some good peter changes

--- Assets/PlayerBend.cs
+++ Assets/PlayerBend.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerBend : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MiScusiActions controls;
+    private GameObject[] PlayerBones;
+
+    public GameObject lowerSpine, midSpine, upperSpine;
+
+    private float MouseYAxisBody;
+    private float bendAdd=0f;
+
+    void Awake()
     {
-        
+        PlayerSetup();
+        controls = new MiScusiActions();
+        controls.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        playerspine();
+    }
+
+    void PlayerSetup()
+    {
+        //Setup/reroute active ragdoll parts to array
+        PlayerBones = new GameObject[]
+        {
+			//0
+			lowerSpine,
+			//1
+			midSpine,
+			//2
+			upperSpine
+        };
+    }
+    void playerspine()
+    {
+        bendAdd += controls.Player.Bend.ReadValue<float>();
+        //Mathf.Clamp(bendAdd,-90,90);
+        if (bendAdd > 90)
+            bendAdd = 90;
+        if (bendAdd < -90)
+            bendAdd = -90;
+        midSpine.transform.localRotation = Quaternion.Euler(bendAdd, 0f, 0f);
+
+        //midSpine.transform.localRotation = Quaternion.Euler(45, 0, 0);
+
+        
+
+           // MouseYAxisBody = controls.Player.Bend.ReadValue<float>();
+            //MouseYAxisBody = MouseYAxisBody * 5;
+            //midSpine.transform.localRotation = Quaternion.Euler(-MouseYAxisBody, 0, 0);
+        
         
     }
 }
