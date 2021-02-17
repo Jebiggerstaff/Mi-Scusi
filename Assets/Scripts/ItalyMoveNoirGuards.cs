ae66b696d8f9ef2df075a284b29722fada4a830f
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-02-17 14:59:24 -0600

    Move noir guards

---
+++ Assets/Scripts/ItalyMoveNoirGuards.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItalyMoveNoirGuards : MonoBehaviour
+{
+    StayStillAIMan AI;
+    public Vector3 moveTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AI = GetComponent<StayStillAIMan>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(checkPlayerCostume())
+        {
+            if(AI == null)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                AI.target = moveTarget;
+                AI.shovesPlayer = false;
+            }
+        }
+    }
+
+
+
+
+    bool checkPlayerCostume()
+    {
+        //TODO: CHECK COSTUME
+
+        return false;
+    }
+}
