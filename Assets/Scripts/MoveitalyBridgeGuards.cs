d358a14f28d7848de9c9aafa6a0a3d4840c9234b
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-02-17 14:53:28 -0600

    Some pushing things

---
+++ Assets/Scripts/MoveitalyBridgeGuards.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveitalyBridgeGuards : MonoBehaviour
+{
+    public Vector3 originalTarget;
+    public Vector3 moveTarget;
+    StayStillAIMan ai;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = GetComponent<StayStillAIMan>();
+        ai.destinations[0] = originalTarget;
+        ai.currentDestination = originalTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(checkPlayerCostume())
+        {
+            ai.target = moveTarget;
+            ai.shovesPlayer = false;
+        }
+        else
+        {
+            ai.target = originalTarget;
+            ai.shovesPlayer = true;
+        }
+    }
+
+
+
+    bool checkPlayerCostume()
+    {
+        //TODO: CHECK PLAYER COSTUME
+
+        return false;
+    }
+}
