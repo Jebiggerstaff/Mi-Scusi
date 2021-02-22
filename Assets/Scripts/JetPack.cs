53542943af616cbe5e72cebd671d9c3d7e792d80
AJBollman <alec.bollman@trojans.dsu.edu>
2021-02-22 08:02:47 -0600

    LUL aj skin and jetpack changes

---
+++ Assets/Scripts/JetPack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPack : MonoBehaviour
+{
+    MiScusiActions controls;
+    public GameObject spine;
+    public int force;
+    private Vector3 directionforce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = new MiScusiActions();
+        controls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controls.Player.Jump.)
+        {
+            directionforce = spine.;
+            spine.GetComponent<Rigidbody>().velocity = directionforce;
+        }
+    }
+}
