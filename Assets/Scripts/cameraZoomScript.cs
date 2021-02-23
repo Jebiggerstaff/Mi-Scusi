0dc14c73ed3d3d56cc2219c691d876bfece696c2
snberanek <36345444+snberanek@users.noreply.github.com>
2021-02-23 13:18:03 -0600

    fademehopework-E
    cthulhu phtagn'

--- Assets/Scripts/cameraZoomScript.cs
+++ Assets/Scripts/cameraZoomScript.cs
@@ -8,28 +8,35 @@ public class cameraZoomScript : MonoBehaviour
     private MiScusiActions controls;
 
     public float timeLeft = 15.0f;
+
+    float badTime;
+
     public Animator animator;
+    public GameObject fadeCube;
 
     private void Start()
     {
         controls = new MiScusiActions();
         controls.Enable();
+        badTime = timeLeft - 3.0f;
     }
 
     void Update()
     {
-
+        
         timeLeft -= Time.deltaTime;
         if (timeLeft <= 0)
         {
             animator.enabled = false;
+            fadeCube.SetActive(false);
         }
 
         else if (controls.Player.Jump.triggered)
         {
-            if (timeLeft <= 10.0f)
+            if (timeLeft <= badTime)
             {
                 animator.enabled = false;
+            fadeCube.SetActive(false);
             }
             
         }
