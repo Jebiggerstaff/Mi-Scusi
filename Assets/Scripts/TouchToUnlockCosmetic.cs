7b87f2efb52b6bb75c2531744241931484971e33
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-03-19 08:29:28 -0500

    yeet

---
+++ Assets/Scripts/TouchToUnlockCosmetic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToUnlockCosmetic : MonoBehaviour
+{
+    string cosmeticName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player_1"))
+        {
+            FindObjectOfType<CosmeticUnlocker>().UnlockOutfit(cosmeticName);
+            Destroy(this.gameObject);
+        }
+
+    }
+}
