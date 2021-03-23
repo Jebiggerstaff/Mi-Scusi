acddf90591fb6445e06d5f93d367a6aed7dcedb3
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-03-22 19:09:20 -0500

    office pipes reverse

---
+++ Assets/SetBoxDirection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBoxDirection : MonoBehaviour
+{
+    public List<WindBox> boxes;
+    public bool goUp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player_1"))
+        {
+            foreach(var b in boxes)
+            {
+                b.SetDirection(goUp);
+            }
+        }
+    }
+}
