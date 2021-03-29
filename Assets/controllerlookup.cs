bf14b30b1d2b51e4f94476ed489d862462cd8cc2
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2021-03-29 13:15:34 -0500

    x

---
+++ Assets/controllerlookup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class controllerlookup : MonoBehaviour
+{
+    private MiScusiActions controls;
+
+    public GameObject spacebar;
+    public GameObject A;
+    public GameObject X;
+
+    private void Awake()
+    {
+        controls = new MiScusiActions();
+        controls.Enable();
+    }
+
+    void Update()
+    {
+        if (Gamepad.current == null)
+        {
+            spacebar.SetActive(true);
+        }
+        else if(Gamepad.current.name== "PlayStation 4 Controller")
+        {
+            X.SetActive(true);
+        }
+        else if(Gamepad.current.name == "XBox One Controller")
+        {
+            A.SetActive(true);
+        }
+    }
+}
