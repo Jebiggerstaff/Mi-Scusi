fdc333714c6ce787e77d7b81f7f115175a30913f
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-04-12 08:50:57 -0500

    moon stuff

---
+++ Assets/Scripts/MoonCubePuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonCubePuzzle : MonoBehaviour
+{
+    public CruiseBtnHelper X;
+    public CruiseBtnHelper Y;
+    public CruiseBtnHelper Z;
+
+    public GameObject cube;
+    public GameObject sampleCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (X.pressed)
+            cube.transform.rotation = Quaternion.Euler(cube.transform.rotation.eulerAngles + new Vector3(30 * Time.deltaTime, 0, 0));
+        if (Y.pressed)
+            cube.transform.rotation = Quaternion.Euler(cube.transform.rotation.eulerAngles + new Vector3(0, 30 * Time.deltaTime, 0));
+        if (Z.pressed)
+            cube.transform.rotation = Quaternion.Euler(cube.transform.rotation.eulerAngles + new Vector3(0, 0, 30 * Time.deltaTime));
+
+        sampleCube.transform.rotation = cube.transform.rotation;
+
+    }
+}
