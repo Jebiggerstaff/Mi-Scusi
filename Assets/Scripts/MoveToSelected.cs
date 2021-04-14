1deea645c76173e2b8909291b71def323343a63c
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-04-14 13:50:28 -0500

    ring on the customization menu

---
+++ Assets/Scripts/MoveToSelected.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MoveToSelected : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = EventSystem.current.currentSelectedGameObject.transform.position;
+    }
+}
