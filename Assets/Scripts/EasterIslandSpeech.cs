030384b7e6a7543f969c246b77667818578c3b05
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-04-14 13:24:47 -0500

    talking heads

---
+++ Assets/Scripts/EasterIslandSpeech.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterIslandSpeech : MonoBehaviour
+{
+    public ImageEffect compareTarget;
+    public GameObject speechStuff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speechStuff.SetActive(compareTarget.enabled);
+    }
+}
