417e8ba3a6732d1d9bf7071bd8fe0f8e586e6905
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-04-23 13:02:14 -0500

    Can scroll thru all customization options now

---
+++ Assets/CustomizeButton.cs
@@ -0,0 +1,53 @@
+﻿using SpeedTutorMainMenuSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomizeButton : MonoBehaviour
+{
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setHat(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetHat(costumeNumber);
+    }
+    public void setShirt(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetShirt(costumeNumber);
+    }
+    public void setCoat(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetCoat(costumeNumber);
+    }
+    public void setPants(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetPants(costumeNumber);
+    }
+    public void setAcc(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetAccessory(costumeNumber);
+    }
+    public void setBackpacl(int costumeNumber)
+    {
+        if (image.color == Color.white)
+            FindObjectOfType<MenuController>().SetBackpack(costumeNumber);
+    }
+}
